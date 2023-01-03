a3d7bbdf31b8c967569740efd1c449e09a753b68
ravenwing <kolodzinski.krzys@gmail.com>
2023-01-03 16:23:50 +0100

    Fix for sending emails (#5)
    Fix for SMTP setup

--- platzky/plugins/sendmail/entrypoint.py
+++ platzky/plugins/sendmail/entrypoint.py
@@ -5,7 +5,7 @@ from functools import partial
 def send_mail(sender_email, password, smtp_server, port, receiver_email, subject, message):
     full_message = f'From: {sender_email}\nTo: {receiver_email}\nSubject: {subject}\n\n{message}'
 
-    server = smtplib.SMTP(smtp_server, port)
+    server = smtplib.SMTP_SSL(smtp_server, port)
     server.ehlo()
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, full_message)
