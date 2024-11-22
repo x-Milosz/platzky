783c5c0bf5af4561716b49a49a8d6f9aaf685de0
Miłosz Nowaczyk <milosz.nowaczyk@protonmail.com>
2024-11-22 13:16:27 +0100

    revert: changed cypress config

--- tests/e2e_tests/cypress.config.js
+++ tests/e2e_tests/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://www.localhost:5000/',
+    baseUrl: 'http://www.platzky.localhost:5000/',
     setupNodeEvents(on, config) {
       return config;
     }
